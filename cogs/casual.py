07cafeaed5e31c1205f588ce6e8ddde73ce642b1
Sen <57877797+cursedsen@users.noreply.github.com>
2025-01-24 12:05:41 +0100

    Add 'about' and 'avatar' commands to Casual Cog for user information and avatar retrieval

--- cogs/casual.py
+++ cogs/casual.py
@@ -1,9 +1,27 @@
 import discord
+
 from discord.ext import commands
 
 class Casual(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        
+    #################################
+    ## About Command
+    #################################
+    @commands.command()
+    async def about(self, ctx):
+        """About Nessy"""
+        embed = discord.Embed(
+            title="✦ About Nessy ✦",
+            description=(
+                "Work in progress"
+            ),
+            color=0x2B2D31
+        )
+        embed.set_thumbnail(url=self.bot.user.avatar.url)
+        embed.set_footer(text=f"Buh")
+        await ctx.send(embed=embed)
         
     #################################
     ## Ping Command
@@ -24,6 +42,17 @@ class Casual(commands.Cog):
         embed.add_field(name="ID", value=f"```{member.id}```")
         embed.add_field(name="Joined", value=f"```{member.joined_at.strftime('%Y-%m-%d')}```")
         embed.set_thumbnail(url=member.avatar.url)
+        await ctx.send(embed=embed)
+    
+    #################################
+    ## Avatar Command
+    #################################
+    @commands.command()
+    async def avatar(self, ctx, member: discord.Member = None):
+        """Get a user's avatar"""
+        member = member or ctx.author
+        embed = discord.Embed(title=f"{member.display_name}'s Avatar", color=member.color)
+        embed.set_image(url=member.avatar.url)
         await ctx.send(embed=embed)
 
 async def setup(bot):
