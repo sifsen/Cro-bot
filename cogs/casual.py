1ad1afdc603e13c3c24cb9c9dc7c0e21d79ee3a0
cursedsen <git@sen.wtf>
2025-01-30 12:43:00 +0100

    add invite and banner command

--- cogs/casual.py
+++ cogs/casual.py
@@ -22,7 +22,7 @@ class Casual(commands.Cog):
         embed.set_thumbnail(url=self.bot.user.avatar.url)
         embed.set_footer(text=f"Buh")
         await ctx.send(embed=embed)
-        
+
     #################################
     ## Issues Command
     ################################
@@ -30,6 +30,14 @@ class Casual(commands.Cog):
     async def issues(self, ctx):
         """Report an issue or request a feature"""
         await ctx.send("Click [this link](https://github.com/CursedSen/Rei-bot/issues) to report an issue or request a feature!")
+
+    #################################
+    ## Invite Rei
+    #################################
+    @commands.command()
+    async def invite(self, ctx):
+        """Invite Rei to your server"""
+        await ctx.send("Click [this link](https://discord.com/oauth2/authorize?client_id=1293508738036142091) to invite me to your server!")
         
     #################################
     ## Ping Command
@@ -63,5 +71,22 @@ class Casual(commands.Cog):
         embed.set_image(url=member.avatar.url)
         await ctx.send(embed=embed)
 
+    #################################
+    ## Banner Command
+    #################################
+    @commands.command()
+    async def banner(self, ctx, member: discord.Member = None):
+        """Get a user's banner"""
+        member = member or ctx.author
+        
+        user = await self.bot.fetch_user(member.id)
+        if not user.banner:
+            await ctx.send(f"{member.display_name} doesn't have a banner!")
+            return
+            
+        embed = discord.Embed(title=f"{member.display_name}'s Banner", color=member.color)
+        embed.set_image(url=user.banner.url)
+        await ctx.send(embed=embed)
+
 async def setup(bot):
-    await bot.add_cog(Casual(bot)) 
+    await bot.add_cog(Casual(bot))
